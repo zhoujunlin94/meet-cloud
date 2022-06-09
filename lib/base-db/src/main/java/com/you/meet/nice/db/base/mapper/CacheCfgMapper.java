0fe8c18c3fa5eda85c7d7569883102007d7aa5f0
zhoujunlin <zhoujunlin@gaodun.com>
2022-06-09 16:22:09 +0800

    init

--- lib/base-db/src/main/java/com/you/meet/nice/db/base/mapper/CacheCfgMapper.java
+++
@@ -1,9 +0,0 @@
-package com.you.meet.nice.db.base.mapper;
-
-import com.you.meet.nice.db.base.model.CacheCfg;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface CacheCfgMapper extends tk.mybatis.mapper.common.Mapper<CacheCfg> {
-
-}

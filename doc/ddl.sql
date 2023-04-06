a607f8095dba6e3ab1b8dc5ecfc02b38de8bf941
zhoujunlin <zjl159753264>
2023-04-06 23:17:18 +0800

    docs

--- doc/ddl.sql
+++ doc/ddl.sql
@@ -2,9 +2,9 @@ CREATE DATABASE IF NOT EXISTS `base` CHARACTER SET 'utf8mb4';
 USE `base`;
 CREATE TABLE IF NOT EXISTS `cache_cfg`
 (
-    `key`   varchar(255) NOT NULL COMMENT '主键，配置key',
-    `value` text         NOT NULL DEFAULT '' COMMENT '配置值',
-    `desc`  varchar(1024)         DEFAULT NULL COMMENT '配置描述',
+    `key`   varchar(128)  NOT NULL COMMENT '主键,配置key',
+    `value` varchar(2048) NOT NULL DEFAULT '' COMMENT '配置值',
+    `desc`  varchar(1024) NOT NULL DEFAULT '' COMMENT '配置描述',
     PRIMARY KEY (`key`)
 ) ENGINE = InnoDB;
 
--- doc/dml.sql
+++ doc/dml.sql
@@ -1,3 +1,1 @@
 USE `base`;
-INSERT INTO `cache_cfg` (`key`, `value`, `desc`)
-VALUES ('xssConfig', '{\r\n	\"excludes\": \"/oauth/token\",\r\n	\"isIncludeRichText\": \"false\"\r\n}', 'xss配置');

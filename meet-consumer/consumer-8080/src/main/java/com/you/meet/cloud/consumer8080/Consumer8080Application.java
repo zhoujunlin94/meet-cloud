78aab051522ed570351d791fbe95d14c743a5f8d
zhoujunlin <zjl159753264>
2023-08-20 15:54:09 +0800

    init

--- meet-consumer/consumer-8080/src/main/java/com/you/meet/cloud/consumer8080/Consumer8080Application.java
+++ meet-consumer/consumer-8080/src/main/java/com/you/meet/cloud/consumer8080/Consumer8080Application.java
@@ -2,12 +2,14 @@ package com.you.meet.cloud.consumer8080;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 /**
  * @author zhoujunlin
  * @date 2023年08月17日 22:59
  * @desc
  */
+@EnableAspectJAutoProxy(exposeProxy = true)
 @SpringBootApplication
 public class Consumer8080Application {
 

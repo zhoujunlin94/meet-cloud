cdfc71577c0717b1539fb6b83c423ac1fec5fd69
zhoujunlin <zhoujunlin@gaodun.com>
2023-12-27 11:41:35 +0800

    discovery

--- meet-consumer/consumer-nacos-discovery/src/main/java/com/you/meet/cloud/consumer/nacos/discovery/controller/ConsumerController.java
+++ meet-consumer/consumer-nacos-discovery/src/main/java/com/you/meet/cloud/consumer/nacos/discovery/controller/ConsumerController.java
@@ -4,11 +4,16 @@ import com.you.meet.cloud.consumer.nacos.discovery.feign.ProviderFeignClient;
 import com.you.meet.nice.common.pojo.JsonResponse;
 import lombok.RequiredArgsConstructor;
 import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalancerClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * @author zhoujunlin
@@ -22,6 +27,7 @@ public class ConsumerController {
 
     private final LoadBalancerClient loadBalancerClient;
     private final RestTemplate restTemplate;
+    private final DiscoveryClient discoveryClient;
     private final ProviderFeignClient providerFeignClient;
 
     @GetMapping("/headers_one")
@@ -44,6 +50,22 @@ public class ConsumerController {
     @GetMapping("/headers_three")
     public JsonResponse getHeader3() {
         return providerFeignClient.headers();
+    }
+
+    @GetMapping("/services")
+    public List<String> discovery() {
+        return discoveryClient.getServices();
+    }
+
+    @GetMapping("/instances")
+    public Map<String, List<ServiceInstance>> instances() {
+        Map<String, List<ServiceInstance>> ret = new HashMap<>();
+        List<String> services = discoveryClient.getServices();
+        for (String service : services) {
+            List<ServiceInstance> instances = discoveryClient.getInstances(service);
+            ret.put(service, instances);
+        }
+        return ret;
     }
 
 }

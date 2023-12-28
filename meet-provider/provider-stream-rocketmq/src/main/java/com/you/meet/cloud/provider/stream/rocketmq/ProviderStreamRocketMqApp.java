c17544569e0874d79780996a159a8e3c43b3839b
zhoujunlin <zhoujunlin@gaodun.com>
2023-12-28 09:47:56 +0800

    discovery

---
+++ meet-provider/provider-stream-rocketmq/src/main/java/com/you/meet/cloud/provider/stream/rocketmq/ProviderStreamRocketMqApp.java
@@ -0,0 +1,18 @@
+package com.you.meet.cloud.provider.stream.rocketmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoujunlin
+ * @date 2023年12月28日 9:34
+ * @desc
+ */
+@SpringBootApplication
+public class ProviderStreamRocketMqApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProviderStreamRocketMqApp.class, args);
+    }
+
+}

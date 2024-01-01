01dfd447df5fd2904f99666a78731ed8eba7b58e
zhoujunlin <zhoujunlin.work@outlook.com>
2024-01-01 20:42:12 +0800

    kafka

---
+++ meet-consumer/consumer-kafka/src/main/java/com/you/meet/cloud/consumer/kafka/ConsumerKafkaApp.java
@@ -0,0 +1,18 @@
+package com.you.meet.cloud.consumer.kafka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zhoujunlin
+ * @date 2023年12月29日 14:52
+ * @desc
+ */
+@SpringBootApplication
+public class ConsumerKafkaApp {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerKafkaApp.class, args);
+    }
+
+}
---
+++ meet-consumer/consumer-kafka/src/main/java/com/you/meet/cloud/consumer/kafka/listener/Demo02Consumer.java
@@ -0,0 +1,44 @@
+package com.you.meet.cloud.consumer.kafka.listener;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+/**
+ * @author zhoujunlin
+ * @date 2022年12月26日 21:35
+ * @desc
+ */
+@Slf4j
+@Component
+public class Demo02Consumer {
+
+    /**
+     * 单个消息消费
+     */
+    /*@KafkaListener(
+            topics = {"TOPIC_DEMO02"},
+            groupId = "TOPIC_DEMO02-consumer-group"
+    )
+    public void onMessage(String message) {
+        log.info("[Demo02Consumer.onMessage][线程编号:{} 消息内容：{}]", Thread.currentThread().getId(), message);
+    }*/
+
+    /**
+     * 批量消费
+     *
+     * @param messages
+     */
+    @KafkaListener(
+            topics = {"TOPIC_DEMO02"},
+            groupId = "TOPIC_DEMO02-consumer-group"
+    )
+    public void onMessage(List<String> messages) {
+        log.info("[Demo02Consumer.onMessage][线程编号:{},消息数量:{} 消息内容:{}]", Thread.currentThread().getId(), messages.size(), messages);
+    }
+
+}
+
+
